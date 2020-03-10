c0e465e0f1bdc1f844aed6180cb58563d4a60cf8
Alexander-Lee Couchy <alexander.couchy@gmail.com>
2020-03-10 19:09:48 +0100

    Post fetching from database operational

---
+++ MOP/Models/LikeModel.swift
@@ -0,0 +1,9 @@
+//
+//  LikeModel.swift
+//  MOP
+//
+//  Created by etud on 10/03/2020.
+//  Copyright © 2020 Groupe Incroyable. All rights reserved.
+//
+
+import Foundation
---
+++ MOP/Models/ReportModel.swift
@@ -0,0 +1,9 @@
+//
+//  ReportModel.swift
+//  MOP
+//
+//  Created by etud on 10/03/2020.
+//  Copyright © 2020 Groupe Incroyable. All rights reserved.
+//
+
+import Foundation
